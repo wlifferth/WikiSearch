0eb1e736c01b1233f704e41c33ec3dc3eb08aea7
Will Lifferth <wlifferth@icloud.com>
2017-12-31 16:21:30 -0600

    Added basic bootstrap style and updateResults

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var getResults = (query, callback) => {
-  var url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${query}&format=json&prop=extracts&exsentences=4`;
+  var url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts%7Cinfo&list=search&titles=pie&exsentences=4&inprop=url&srsearch=${query}`;
   console.log(url);
     $.getJSON({'url': url}, callback);
 }
 
 var updateResults = (apiResult) => {
-  results = apiResult['query']['search'].map((item) => { return item['title']; });
-  $("#results").text(results.toString());
+  console.log(apiResult);
+  results = apiResult['query']['search'];
+  for(result of results) {
+    appendEntry(result['title'], result['snippet']);
+  }
+  //$("#results").text(results.toString());
+}
+
+var appendEntry = (title, excerpt) => {
+  console.log(`<div class="result">
+    <h3>
+    ${title}
+    </h3>
+    <p>
+    ${excerpt}
+    </p>
+  </div>`);
+
+  $("#results").append(
+    `<div class="result">
+      <h3>
+      ${title}
+      </h3>
+      <p>
+      ${excerpt}
+      </p>
+    </div>`
+);
 }
 
 $(document).ready(() => {
